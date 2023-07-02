263468a79050dcdd620cb2371f04d63742b200b8
Kevin McAleer <kevinmcaleer@gmail.com>
2023-07-02 10:40:58 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+scene.onOverlapTile(SpriteKind.Player, assets.tile`exit`, function (sprite, location) {
+    game.gameOver(true)
+})
 scene.setBackgroundColor(0)
 tiles.setCurrentTilemap(tilemap`level2`)
 let mySprite = sprites.create(assets.image`Elf`, SpriteKind.Player)
+let mySprite2 = sprites.create(assets.image`ghost`, SpriteKind.Enemy)
 tiles.placeOnRandomTile(mySprite, assets.tile`start`)
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
+game.onUpdateInterval(1000, function () {
+    mySprite2.setVelocity(randint(-50, 50), randint(-50, 50))
+})
